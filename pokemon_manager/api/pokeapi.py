38adee70ef60cf7a001c712c29cf9b5ee63f2174
Diana <dianaprada20@gmail.com>
2024-01-19 13:37:07 -0500

    feature(pokemon_manager.api.pokeapi):get pokemon images function

--- pokemon_manager/api/pokeapi.py
+++ pokemon_manager/api/pokeapi.py
@@ -1,5 +1,32 @@
 import requests
+import json
 
-def pokemon_list():
-    pokemon = requests.get("https://pokeapi.co/api/v2/pokemon")
+def pokemon_list() -> dict:
+    try:
+        res = requests.get("https://pokeapi.co/api/v2/pokemon")
+        res = json.loads(res.content)
+    except Exception as ex:
+        print(ex) #TODO:You need to manage this exception
+
+    return res
+
+
+def get_pokemon(url: str) -> dict:
+    res = requests.get(url)
+    res = json.loads(res.content)
+
+    pokemon = {
+        "name": res["forms"][0]["name"],
+        "image": res["sprites"]["back_default"]
+    }
+
     return pokemon
+
+
+def get_pokemon_images(url: str) -> dict:
+    res = requests.get(url)
+    res = json.loads(res.content)
+
+    images = res["sprites"]
+
+    return images
