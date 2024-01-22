412f70eefc332193ed372a169c234ab01005e662
Diana <dianaprada20@gmail.com>
2024-01-22 11:18:17 -0500

    feature(pokemon manager.services):add pokeapi services

--- pokemon_manager/services.py
+++ pokemon_manager/services.py
@@ -1,1 +1,11 @@
 # public functions to share with anothers internal applications
+from pokemon_manager.api.pokeapi import (
+    pokemon_list,
+    get_pokemon_images,
+)
+
+def get_pokemon_list_with_pagination():
+    return pokemon_list()
+
+def get_pokemon_images():
+    return get_pokemon_images()
