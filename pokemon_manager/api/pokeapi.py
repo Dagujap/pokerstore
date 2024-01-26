412073782ba25e293dd2ecc46d2b3c4faf503c8a
Marcelo LP <marcelo@Marcelos-MacBook-Pro.local>
2024-01-26 16:55:15 +0100

    feature(pokemon_manager.api.pokedex): remove json import

--- pokemon_manager/api/pokeapi.py
+++ pokemon_manager/api/pokeapi.py
@@ -1,32 +1,25 @@
 import requests
-import json
+
 
 def pokemon_list() -> dict:
     #TODO:¨return pagination
-    try:
-        res = requests.get("https://pokeapi.co/api/v2/pokemon")
-        res = json.loads(res.content)
-    except Exception as ex:
-        print(ex) #TODO:You need to manage this exception
+    res = requests.get("https://pokeapi.co/api/v2/pokemon")
+    res = res.json()
 
     return res
 
-
 def get_pokemon(url: str) -> dict:
     res = requests.get(url)
-    res = json.loads(res.content)
+    res = res.json()
 
     pokemon = {
         "name": res["forms"][0]["name"],
         "image": res["sprites"]["back_default"]
     }
 
-    return pokemon
-
-
 def get_pokemon_images(url: str) -> dict:
     res = requests.get(url)
-    res = json.loads(res.content)
+    res = res.json()
 
     images = res["sprites"]
 
